982dbd31bc6bdae7b60d3511de81a9cd7e471a81
Dylan Grinboju <98737394+Dylan-Grinboju@users.noreply.github.com>
2026-01-16 23:36:37 +0200

    saves the surge high scores

--- Patches.cs
+++ Patches.cs
@@ -2,6 +2,8 @@ using HarmonyLib;
 using Silk;
 using Logger = Silk.Logger;
 using UnityEngine.SceneManagement;
+using System;
+using System.Reflection;
 
 namespace SpiderSurge
 {
@@ -15,6 +17,13 @@ namespace SpiderSurge
             {
                 SurgeModeManager.Instance.SetActive(true);
                 Logger.LogInfo("Surge mode started");
+                // Refresh high score display to show Surge scores
+                var eventField = typeof(SurvivalMode).GetField("onHighScoreUpdated", BindingFlags.Public | BindingFlags.Static);
+                if (eventField != null)
+                {
+                    var action = (Action<int>)eventField.GetValue(null);
+                    action?.Invoke(SurvivalMode.instance.GetHighScore());
+                }
             }
         }
     }
@@ -44,6 +53,50 @@ namespace SpiderSurge
                 Logger.LogInfo("Surge mode ended (lobby entered)");
                 SurgeModeManager.Instance.SetActive(false);
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(SurvivalConfig), "GetHighScore")]
+    public class SurvivalConfig_GetHighScore_Patch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(SurvivalConfig __instance, ref float __result)
+        {
+            if (ModConfig.enableSurgeMode)
+            {
+                __result = GameSaveWrapper.Instance.Load<float>(__instance.name + "-SurgeHS", 0f);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SurvivalConfig), "SetHighScore")]
+    public class SurvivalConfig_SetHighScore_Patch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(SurvivalConfig __instance, float score)
+        {
+            if (ModConfig.enableSurgeMode)
+            {
+                GameSaveWrapper.Instance.Save<float>(__instance.name + "-SurgeHS", score);
+                return false; // Prevent original save
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(EOSLeaderboards), "SetRecord", new Type[] { typeof(uint), typeof(int) })]
+    public class EOSLeaderboards_SetRecord_Patch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            if (SurgeModeManager.Instance.IsActive)
+            {
+                return false; // Skip leaderboard submission for Surge mode
+            }
+            return true;
         }
     }
 }
